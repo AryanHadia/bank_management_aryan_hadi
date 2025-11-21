cd137dd82c8d3d11ef06b8faff71d6bcb49bebb4
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-11-21 22:08:54 +0100

    Fix formatting issues in utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ import hashlib
 
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
+
+
 def check_password(hashed_password, plain_password):
     if hashed_password == hash_password(plain_password):
         return True
     else:
         return False
     
+
