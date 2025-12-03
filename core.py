c7e9a2010d5c7c3f6028dcf8196670526e8c0b99
AryanHadia <aryan.hd09@gmail.com>
2025-12-03 19:55:27 +0330

    Update core.py

--- core.py
+++ core.py
@@ -1,8 +1,7 @@
-from models import customer, Account
+from models import customer, Account , admin_data
 from utils import hash_password, check_password
 from data_base import get_session
 import random
-
 
 class admin_panel:
     def __init__(self):
@@ -21,83 +20,126 @@ class admin_panel:
         print(f"Customer Address: {new_customer.address}")
         return new_customer
     
+    
     def create_account(self, customer_id, account_number , account_type, account_balance , pin):
-        customer=self.session.get(customer, customer_id)
-        if not customer:
-            raise exception("Customer not found")
+        db_customer = self.session.get(customer, customer_id)
+        if not db_customer:
+            raise ValueError("Customer not found")
         hashed_pin = hash_password(pin)
-        account=Account(customer_id=customer.id, account_number=account_number, account_type=account_type, account_balance=account_balance, pin=hashed_pin)
+        account = Account(customer_id=db_customer.id, account_number=account_number, account_type=account_type, balance=account_balance, pin=hashed_pin)
         self.session.add(account)
         self.session.commit()
         print(f"Account {account_number} created successfully")
         print(f"Account ID: {account.id}")
         print(f"Account Type: {account.account_type}")
-        print(f"Account Balance: {account.account_balance}")
+        print(f"Account Balance: {account.balance}")
         return account
 
+    
     def delete_account(self,account_id):
         account=self.session.get(Account, account_id)
         if not account:
-            raise exception("Account not found")
+            raise ValueError("Account not found")
         self.session.delete(account)
         self.session.commit()
         print(f"Account {account_id} deleted successfully")
         return account
     
+    
     def show_balance(self,account_id):
         account=self.session.get(Account, account_id)
         if not account:
-            raise exception("Account not found")
-        print(f"Account Balance: {account.account_balance}")
+            raise ValueError("Account not found")
+        print(f"Account Balance: {account.balance}")
         return account
+    
     
     def deposit(self,account_id, amount):
         account=self.session.get(Account, account_id)
         if not account:
-            raise exception("Account not found")
-        acount.balance += amount
+            raise ValueError("Account not found")
+        account.balance += amount
         self.session.commit()
         return account
+    
     
     def withdraw(self,account_id, amount):
         account=self.session.get(Account, account_id)
         if not account:
             raise exception("Account not found")
         if account.balance < amount:
-            raise exception("Insufficient balance")
+            raise ValueError("Insufficient balance")
         account.balance -= amount
         self.session.commit()
         return account
         
     
+    
     def transfer(self,account_id,to_account_id, amount): # transfer from account_id to to_account_id (one account to another account)
         account1=self.session.get(Account, account_id)
         if not account1:
-            raise exception("Account not found")
+            raise ValueError("Account not found")
         account2=self.session.get(Account, to_account_id)
         if not account2:
-            raise exception("Account not found")
+            raise ValueError("Account not found")
         if account1.balance < amount:
-            raise exception("Insufficient balance")
+            raise ValueError("Insufficient balance")
         account1.balance -= amount
         account2.balance += amount
         self.session.commit()
         return account1
 
+    
     def show_transactions(self,account_id):
         account=self.session.get(Account, account_id)
         if not account:
-            raise exception("Account not found")
+            raise ValueError("Account not found")
         transactions=account.transactions
         for transaction in transactions:
             print(transaction)
         return transactions
 
+    
     def make_account_number(self):
         num = random.randint(1000000000, 9999999999)
         exists = self.session.query(Account).filter_by(account_number=num).first()
         if exists:
             return False
         else:
-
             return num
+        
+    def remove_customer(self, name):
+        customer=self.session.query(customer).filter_by(name=name).first()
+        if not customer:
+            raise ValueError("Customer not found")
+        self.session.delete(customer)
+        self.session.commit()
+        print(f"Customer {name} deleted successfully")
+        return customer
+
+    def create_admin(self, ad_username, ad_password):
+        hashed_password = hash_password(ad_password)
+        admin = admin_data(username=ad_username, password=hashed_password)
+        self.session.add(admin)
+        self.session.commit()
+        print(f"Admin {ad_username} created successfully")
+        return admin
+
+    def remove_admin(self, ad_username):
+        admin=self.session.query(admin_data).filter_by(username=ad_username).first()
+        if not admin:
+            raise ValueError("Admin not found")
+        self.session.delete(admin)
+        self.session.commit()
+        print(f"Admin {ad_username} deleted successfully")
+        return admin
+
+    def admin_login(self, ad_username_login, ad_password_login):
+        admin=self.session.query(admin_data).filter_by(username=ad_username_login).first()
+        if not admin:
+            raise ValueError("Admin not found")
+        if check_password(admin.password, ad_password_login) == False:
+            raise ValueError("Invalid password")
+        elif check_password(admin.password, ad_password_login) == True: 
+            return admin
+        print(f"Admin {ad_username_login} logged in successfully")
