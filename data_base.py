06186013027660c01fa96bb9c6502d393c4fd973
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-12-03 17:49:18 +0100

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker,declarative_base
 engine = create_engine("mysql+mysqlconnector://root:root1234@localhost",echo=False ,future=True)
 base = declarative_base()
 session = sessionmaker(bind=engine ,autoflush=False , future=True)
-
 def get_session():
     return session()
+
 
 
