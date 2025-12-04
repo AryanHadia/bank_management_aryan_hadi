6f44ca576cac65bbfd701c313b54a427a7b86c34
AryanHadia <aryan.hd09@gmail.com>
2025-12-04 18:10:46 +0330

    Update models.py

--- models.py
+++ models.py
@@ -1,19 +1,20 @@
 # class ke vasl behshe be soton database
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column , String , Integer , Float , ForeignKey , DateTime
+from sqlalchemy import Column , String , Integer , Float , ForeignKey , DateTime , LargeBinary
 from datetime import datetime
 from data_base import base
 
 class customer(base):
-    __tablename__ = "customers"
+    __tablename__ = "customer"
     id = Column(Integer , primary_key=True , autoincrement=True)
     email = Column(String , unique=True)
     name = Column(String , nullable=False)
     age = Column(Integer , nullable=False)
     phone_number = Column(String , nullable=False)
     address = Column(String , nullable=False)
+    
 
-    accounts = relationship("Account" , back_populates="customer")
+    accounts = relationship("Account", back_populates="customer", cascade="all, delete-orphan")
     
     """ customer data base """
 
@@ -22,31 +23,42 @@ class customer(base):
     def __repr__(self):
         return f"<id={self.id} , email={self.email}>"
 
+
+
+
 class Account(base):
     __tablename__ = "accounts"
     id = Column(Integer , primary_key=True , autoincrement=True)
     account_number = Column(String , unique=True)
     account_type = Column(String , nullable=False)
     balance = Column(Float , nullable=False)
-    customer_id = Column(Integer , ForeignKey("customers.id"))
+    customer_id = Column(Integer , ForeignKey("customer.id"))   
     pin = Column(String , nullable=False)
 
     # relationships
-    customer = relationship("customer" , back_populates="accounts")
+    customer = relationship("customer", back_populates="accounts", foreign_keys=[customer_id])
     transactions = relationship("Transaction" , back_populates="account")
+
+
+
+
 
 class admin_data(base):
     __tablename__= "admin_data"
     id = Column(Integer , primary_key=True , autoincrement=True)
     username = Column(String , unique=True)
     password = Column(String , nullable=False)
+    profile_image = Column(LargeBinary , nullable=True)
+
+
+
 
 class Transaction(base):
     __tablename__ = "transactions"
     id = Column(Integer , primary_key=True , autoincrement=True)
     account_id = Column(Integer , ForeignKey("accounts.id") , nullable=False)
     amount = Column(Float , nullable=False)
-    created_at = Column(DateTime , default=datetime.utcnow)
+    created_at = Column(DateTime , default=datetime.now)
 
     account = relationship("Account" , back_populates="transactions")
 
