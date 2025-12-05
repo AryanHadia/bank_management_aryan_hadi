b6f750ab3e8050fa61f668ba0aef121f4092aa37
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-12-05 21:08:40 +0100

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -4,13 +4,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine("mysql+mysqlconnector://aryan:pythonint12@localhost:3306", echo=False, future=True)
 
-
 with engine.begin() as conn:
     conn.execute(text("CREATE DATABASE IF NOT EXISTS bank_db"))
-
 
 engine = create_engine("mysql+mysqlconnector://aryan:pythonint12@localhost:3306/bank_db", echo=False, future=True)
 
 base = declarative_base()
 SessionLocal = sessionmaker(bind=engine, autoflush=False, future=True)
 session = SessionLocal()
+
