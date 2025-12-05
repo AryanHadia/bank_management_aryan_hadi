281c57e8811259f1caa71a3f23acb45919e1156e
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-12-05 21:07:08 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import hashlib
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
-
 def check_password(hashed_password, plain_password):
     if hashed_password == hash_password(plain_password):
         return True
