3ec94be3e8bd70c55fd44952392c33f96775b019
AryanHadia <aryan.hd09@gmail.com>
2025-12-17 20:56:17 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import hashlib
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
+
 def check_password(hashed_password, plain_password):
     if hashed_password == hash_password(plain_password):
         return True
