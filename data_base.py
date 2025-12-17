8a7af3c96a1de224d81570bc59b2da1677020e9d
AryanHadia <aryan.hd09@gmail.com>
2025-12-17 20:55:23 +0330

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+'''
+just connectong to database
+'''
 
 engine = create_engine("mysql+mysqlconnector://aryan:pythonint12@localhost:3306", echo=False, future=True)
 
+
 with engine.begin() as conn:
     conn.execute(text("CREATE DATABASE IF NOT EXISTS bank_db"))
+
 
 engine = create_engine("mysql+mysqlconnector://aryan:pythonint12@localhost:3306/bank_db", echo=False, future=True)
 
